8fa453cfaab3c7aef9768d53d589a5ef9d31e4f3
Patrick West <pwest@dco.tw.rpi.edu>
2016-04-02 23:48:46 -0600

    The servive now generates a PNAS citation
    Given a DCO-ID we generate a citation in the PNAS format

--- php/api.php
+++ php/api.php
@@ -2,122 +2,220 @@
 // This is the API, 2 possibilities: show the app list or show a specific app by id.
 // This would normally be pulled from a database but for demo purposes, I will be hardcoding the return values.
 
+// make the curl sparql query request
 function request($url)
 {
+    // is curl installed?
+    if (!function_exists('curl_init')){
+        die('CURL is not installed!');
+    }
 
-  // is curl installed?
-  if (!function_exists('curl_init')){
-    die('CURL is not installed!');
-  }
+    // get curl handle
+    $ch= curl_init();
 
-  // get curl handle
-  $ch= curl_init();
+    // set request url
+    curl_setopt($ch, CURLOPT_URL, $url);
 
-  // set request url
-  curl_setopt($ch,
-    CURLOPT_URL,
-    $url);
+    // return response, don't print/echo
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-  // return response, don't print/echo
-  curl_setopt($ch,
-    CURLOPT_RETURNTRANSFER,
-    true);
+    /*
+    Here you find more options for curl:
+    http://www.php.net/curl_setopt
+    */
 
-  /*
-  Here you find more options for curl:
-  http://www.php.net/curl_setopt
-  */
+    $response = curl_exec($ch);
 
-  $response = curl_exec($ch);
+    curl_close($ch);
 
-  curl_close($ch);
-
-  return $response;
+    return $response;
 }
-
 
 function get_citation_by_id($id)
 {
-  $format = 'json';
+    $format = 'json';
 
-  $query = "
-            PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
-            PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>
-            PREFIX owl:   <http://www.w3.org/2002/07/owl#>
-            PREFIX swrl:  <http://www.w3.org/2003/11/swrl#>
-            PREFIX swrlb: <http://www.w3.org/2003/11/swrlb#>
-            PREFIX vitro: <http://vitro.mannlib.cornell.edu/ns/vitro/0.7#>
-            PREFIX bibo: <http://purl.org/ontology/bibo/>
-            PREFIX c4o: <http://purl.org/spar/c4o/>
-            PREFIX cito: <http://purl.org/spar/cito/>
-            PREFIX dcat: <http://www.w3.org/ns/dcat#>
-            PREFIX dco: <http://info.deepcarbon.net/schema#>
-            PREFIX event: <http://purl.org/NET/c4dm/event.owl#>
-            PREFIX fabio: <http://purl.org/spar/fabio/>
-            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-            PREFIX geo: <http://aims.fao.org/aos/geopolitical.owl#>
-            PREFIX p.1: <http://purl.org/dc/elements/1.1/>
-            PREFIX p.2: <http://purl.org/dc/terms/>
-            PREFIX obo: <http://purl.obolibrary.org/obo/>
-            PREFIX ocrer: <http://purl.org/net/OCRe/research.owl#>
-            PREFIX ocresd: <http://purl.org/net/OCRe/study_design.owl#>
-            PREFIX p.3: <http://vivoweb.org/ontology/provenance-support#>
-            PREFIX prov: <http://www.w3.org/ns/prov#>
-            PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-            PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
-            PREFIX vitro-public: <http://vitro.mannlib.cornell.edu/ns/vitro/public#>
-            PREFIX vivo: <http://vivoweb.org/ontology/core#>
-            PREFIX scires: <http://vivoweb.org/ontology/scientific-research#>
+$query = "
+PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd:   <http://www.w3.org/2001/XMLSchema#>
+PREFIX owl:   <http://www.w3.org/2002/07/owl#>
+PREFIX bibo: <http://purl.org/ontology/bibo/>
+PREFIX dco: <http://info.deepcarbon.net/schema#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX vivo: <http://vivoweb.org/ontology/core#>
 
-            SELECT ?label ?dcoIdObject
-            WHERE
-            {
-                  ?dcoIdObject a dco:DCOID .
-                  ?dcoIdObject rdfs:label '"
-            . $id
-            . "' .
-                  ?p dco:hasDcoId ?dcoIdObject .
-                  ?p rdfs:label ?label .
-            }
-            ";
-
-// DCO-ID for test: 11121/3524-1246-5132-9229-CC
-
-  $searchUrl = 'http://deepcarbon.tw.rpi.edu:3030/VIVO/query?'
-    .'query='.urlencode($query)
-    .'&format='.$format;
-
-  $responseArray = json_decode(request($searchUrl), true);
-
-  return $responseArray["results"]["bindings"];
+DESCRIBE ?p ?dcoIdObject ?author_role ?author ?journal
+WHERE
+{
+    ?dcoIdObject a dco:DCOID .
+    ?dcoIdObject rdfs:label '11121/3524-1246-5132-9229-CC' .
+    ?p dco:hasDcoId ?dcoIdObject .
+    ?dcoIdObject rdfs:label '" . $id . "' .
+    ?p vivo:relatedBy ?author_role .
+    ?author_role a vivo:Authorship .
+    ?author_role vivo:relates ?author .
+    ?author a foaf:Person .
+    OPTIONAL {
+        ?doc vivo:hasPublicationVenue ?journal .
+        ?journal a bibo:Journal .
+    }
 }
+";
 
+    // DCO-ID for test: 11121/3524-1246-5132-9229-CC
 
-$possible_url = array("get_citation");
+    $searchUrl = 'http://deepcarbon.tw.rpi.edu:3030/VIVO/query?'
+                .'query='.urlencode($query)
+                .'&output='.$format;
+
+    $responseArray = json_decode(request($searchUrl), true);
+
+    return $responseArray;
+}
 
 $value = "An error has occurred";
 
+$dcoid = "";
+
 if (isset($_GET["get_citation"]))
 {
-  $value = get_citation_by_id($_GET["get_citation"]);
+    $dcoid = $_GET["get_citation"];
 }
 else if (isset($_GET["action"]))
 {
-  switch ($_GET["action"])
+    switch ($_GET["action"])
     {
-      case "get_citation":
-        if (isset($_GET["id"]))
-          $value = get_citation_by_id($_GET["id"]);
-        else
-          $value = "Missing argument";
-        break;
-      default:
-        $value = $_GET["action"];
+        case "get_citation":
+            if (isset($_GET["id"]))
+                $dcoid = $_GET["id"];
+            else
+                $value = "Missing argument";
+            break;
+        default:
+            $value = $_GET["action"];
     }
 }
 
-//return JSON array
-exit(json_encode($value));
+if( !empty( $dcoid ) )
+    $value = get_citation_by_id($dcoid);
+
+// build the full dcoid
+$dx = "http://dx.deepcarbon.net/".$dcoid;
+
+// find the publication object in the json document
+$pub=null;
+foreach( $value as $puburi => $thingy )
+{
+    if(isset($thingy["http://info.deepcarbon.net/schema#hasDcoId"]) && $thingy["http://info.deepcarbon.net/schema#hasDcoId"][0]["value"] == $dx)
+    {
+        $pub = $thingy ;
+        break ;
+    }
+}
+
+// get the list of authors. pub -> relatedBy -> authorRole -> relates -> person
+// there's also a relates back to the publication so don't want that one
+$authors = array();
+
+// if there's only one author then there won't be a rank, so start with 0. Otherwise rank starts with 1
+$rank = 0 ;
+if( isset( $pub["http://vivoweb.org/ontology/core#relatedBy"] ) )
+{
+    $authorRoles = $pub["http://vivoweb.org/ontology/core#relatedBy"];
+    foreach( $authorRoles as $role )
+    {
+        $roleuri = $role["value"];
+        $relates = $value[$roleuri]["http://vivoweb.org/ontology/core#relates"];
+        if( isset( $value[$roleuri]["http://vivoweb.org/ontology/core#rank"] ) )
+        {
+            $rank = $value[$roleuri]["http://vivoweb.org/ontology/core#rank"][0]["value"];
+        }
+        $author = null ;
+        foreach( $relates as $relate )
+        {
+            if( $relate["value"] != $puburi )
+            {
+                $author = $value[$relate["value"]];
+                $name = $author["http://www.w3.org/2000/01/rdf-schema#label"][0]["value"];
+                $authors[intval($rank)] = $name ;
+            }
+        }
+    }
+}
+
+// now we generate the citation
+$citation = "";
+
+// the authors go first and should be in order
+$isfirst = true ;
+foreach($authors as $key => $author)
+{
+    $names = explode(",", $author);
+    if( !$isfirst ) $citation .= " ";
+    $isfirst = false ;
+    $citation .= trim($names[0]) . ", " . substr(trim($names[1]),0,1) . ",";
+}
+
+// if there's a year then add it
+if( isset( $pub["http://info.deepcarbon.net/schema#yearOfPublicationYear"] ) )
+{
+    $citation .= " (" . $pub["http://info.deepcarbon.net/schema#yearOfPublicationYear"][0]["value"] . ") " ;
+} else if( isset( $pub["http://info.deepcarbon.net/schema#yearOfPublication"] ) )
+{
+    $citation .= " (" . $pub["http://info.deepcarbon.net/schema#yearOfPublication"][0]["value"] . ")" ;
+}
+
+// if there's a doi then use it as the publication's link. Otherwise use the dcoid.
+if( isset( $pub["http://purl.org/ontology/bibo/doi"] ) )
+{
+    $link = "https://dx.doi.org/" . $pub["http://purl.org/ontology/bibo/doi"][0]["value"] ;
+} else {
+    $link = $dx ;
+}
+
+// add the title with the link
+$citation .= " <a href=\"$link\">" . $pub["http://www.w3.org/2000/01/rdf-schema#label"][0]["value"] . "</a>.";
+
+// if there's a publication venue (journal, book or whatever) then add it
+if( isset( $pub["http://vivoweb.org/ontology/core#hasPublicationVenue"] ) )
+{
+    $venue = $value[$pub["http://vivoweb.org/ontology/core#hasPublicationVenue"][0]["value"]] ;
+    $venueLabel = $venue["http://www.w3.org/2000/01/rdf-schema#label"][0]["value"];
+    $citation .= " " . $venueLabel ;
+}
+
+// volue(issue):start-end
+if( isset( $pub["http://purl.org/ontology/bibo/volume"] ) )
+{
+    $volume = $pub["http://purl.org/ontology/bibo/volume"][0]["value"];
+    $citation .= " " . $volume ;
+}
+
+if( isset( $pub["http://purl.org/ontology/bibo/issue"] ) )
+{
+    $issue = $pub["http://purl.org/ontology/bibo/issue"][0]["value"];
+    $citation .= "(" . $issue . ")";
+}
+
+if( isset( $pub["http://purl.org/ontology/bibo/pageStart"] ) )
+{
+    $start = $pub["http://purl.org/ontology/bibo/pageStart"][0]["value"];
+}
+
+if( isset( $pub["http://purl.org/ontology/bibo/pageEnd"] ) )
+{
+    $end = $pub["http://purl.org/ontology/bibo/pageEnd"][0]["value"];
+}
+if( isset($start) || isset($end) )
+{
+    $citation .= ":" ;
+    if( isset($start) ) $citation .= $start ;
+    if( isset($end) ) $citation .= "-" . $end ;
+}
+
+// and we're done, exit with the generated citation
+//exit(json_encode($pub));
+exit($citation);
 
 ?>
